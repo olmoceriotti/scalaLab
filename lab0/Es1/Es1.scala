bf88751ae3c11f1b8c8b5b4950c0ead6d63b4a68
Olmo Ceriotti <olmoceriotti@gmail.com>
2023-10-11 09:10:01 +0200

    First lab

---
+++ lab0/Es1/Es1.scala
@@ -0,0 +1,14 @@
+import scala.collection.immutable.Range;
+object Es1 {
+    def main(args: Array[String]) = {
+        if(args.length > 0 && args.length < 4){
+            val begin = if(args.length == 1) 0 else Integer.parseInt(args(0));
+            val end = if(args.length ==  1) Integer.parseInt(args(0)) else Integer.parseInt(args(1));
+            val step = if(args.length == 3) Integer.parseInt(args(2)) else 1;
+            println(Range(begin, end, step).toList.mkString(" "));
+        }else{
+            println(0);
+        }
+        
+    }
+}
---
+++ lab0/Es2/Es2.scala
@@ -0,0 +1,12 @@
+object Es2 {
+    def main(args: Array[String]) = {
+        if(args.length == 1){
+            var message = args(0);
+            var result : String = "";
+            for(char <- message){
+                result = result + ((char - ('a' -1) + 13) % 26 + ('a' -1)).toChar;
+            }
+            println(result);
+        }
+    }
+}
